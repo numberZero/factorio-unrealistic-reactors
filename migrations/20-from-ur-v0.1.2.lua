64d8459652a730d219d5a97b9011ac28c6c7beb9
numzero <numzer0@yandex.ru>
2024-07-27 16:32:03 +0300

    Fix a migration

--- migrations/20-from-ur-v0.1.2.lua
+++ migrations/20-from-ur-v0.1.2.lua
@@ -1,5 +1,5 @@
 for _, reactor in pairs(global.reactors) do
     reactor.core = nil
     reactor.core_id = nil
-    reactor.entity.temperature = reactor.signals.parameters.core_temperature
+    reactor.entity.temperature = reactor.signals.parameters.core_temperature.count
 end
